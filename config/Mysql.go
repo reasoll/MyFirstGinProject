2e377c7cbce9f0d02bb4866f5613c6f54dcb1197
wangrui <1257983649@qq.com>
2020-05-06 17:07:08 +0800

    换成Los的数据库， mysql8.0

--- config/Mysql.go
+++ config/Mysql.go
@@ -3,21 +3,21 @@ package config
 import _ "gopkg.in/yaml.v2"
 
 type Mysql struct {
-	Addr		 string	`yaml:"Addr"`
-	Port		 string	`yaml:"Port"`
-	UserNmae	 string	`yaml:"UserName"`
-	Password	 string	`yaml:"Password"`
-	DataBaseName string	`yaml:"DataBaseName"`
+	Addr         string `yaml:"Addr"`
+	Port         string `yaml:"Port"`
+	UserNmae     string `yaml:"UserName"`
+	Password     string `yaml:"Password"`
+	DataBaseName string `yaml:"DataBaseName"`
 }
 
-func (mysql *Mysql)DefaultMySqlConfig() {
-	mysql.Addr = "172.16.1.66"
+func (mysql *Mysql) DefaultMySqlConfig() {
+	mysql.Addr = "23.234.252.205"
 	mysql.Port = "3306"
-	mysql.DataBaseName = "outsystems"
+	mysql.DataBaseName = "go_database"
 	mysql.UserNmae = "root"
-	mysql.Password = "root"
+	mysql.Password = "1257983649"
 }
 
 func (mysql *Mysql) InitMysqlConfig() {
-	mysql.DefaultMySqlConfig();
+	mysql.DefaultMySqlConfig()
 }
